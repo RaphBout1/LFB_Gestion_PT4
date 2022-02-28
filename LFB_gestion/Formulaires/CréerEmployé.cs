d50abf96bb0d83e24b4c429ba51b40097fc71873
bdimchev <bdimchev@INFO-207-12>
2022-02-28 14:57:14 +0100

    add "connection.close" et utilisation de la classe outils

--- LFB_gestion/Formulaires/CréerEmployé.cs
+++ LFB_gestion/Formulaires/CréerEmployé.cs
@@ -51,9 +51,10 @@ namespace LFB_gestion
                             command.Parameters.AddWithValue("@prenom", prénom_TextBox.Text);
                             command.Parameters.AddWithValue("@nom", nom_textBox.Text);
                             command.Parameters.AddWithValue("@tel", tel_textBox.Text);
-                            command.Parameters.AddWithValue("@mdp", crypter(mdp_textBox.Text));
+                            command.Parameters.AddWithValue("@mdp", Outils.crypter(mdp_textBox.Text));
                             command.ExecuteNonQuery();
                             MessageBox.Show("Utilisateur crée avec succés");
+                            connexion.Close();
                         }
                         catch (Exception ex)
                         {
@@ -153,38 +154,5 @@ namespace LFB_gestion
             };
             return Result;
         }
-
-
-        #region à supprimer pour l'utiliser depuis la classe Tools
-        private static string Key = "ENGn*fa}T&39.349&tgS";
-
-        /// <summary>
-        /// Crypte un string
-        /// </summary>
-        /// <param name="text">Le string à crypter</param>
-        /// <returns>Renvoie un string crypté</returns>
-        public static string crypter(string text)
-        {
-            if (string.IsNullOrEmpty(text)) return "";
-            string res = text + Key;
-            var textBytes = Encoding.UTF8.GetBytes(res);
-            return Convert.ToBase64String(textBytes);
-        }
-
-        /// <summary>
-        /// Décrypter un string
-        /// </summary>
-        /// <param name="base64EncodeData">Le string à décrypter</param>
-        /// <returns>Renvoie le string décrypté</returns>
-        public static string décrypter(string base64EncodeData)
-        {
-            if (string.IsNullOrEmpty(base64EncodeData)) return "";
-            var base64EncodeBytes = Convert.FromBase64String(base64EncodeData);
-            var res = Encoding.UTF8.GetString(base64EncodeBytes);
-            res = res.Substring(0, res.Length - Key.Length);
-            return res;
-        }
-        #endregion
-
     }
 }
--- LFB_gestion/Program.cs
+++ LFB_gestion/Program.cs
@@ -16,7 +16,7 @@ namespace LFB_gestion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CréerEmployé());
+            Application.Run(new Connexion());
         }
     }
 }
