cd8d097845b038e63dceec97f6b05102a6f5425f
bdimchev <bdimchev@INFO-207-12>
2022-03-02 12:37:08 +0100

    réservation ok :)))))))))))) classe client

---
+++ LFB_gestion/Classes/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFB_gestion.Classes
+{
+    internal class Client
+    {
+        public int id;
+
+        public string nom;
+
+        public string prenom;
+
+        public string mail;
+
+        public Client(int id, string nom, string prenom, string mail)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.mail = mail;
+        }
+        
+        public override string ToString()
+        {
+            return prenom + " " + nom;
+        }
+    }
+}
