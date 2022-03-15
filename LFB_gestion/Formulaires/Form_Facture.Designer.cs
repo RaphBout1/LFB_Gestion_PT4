14c25b862125ef3ff5822e2fb95a661482701f85
bdimchev <bdimchev@INFO-207-12>
2022-03-15 17:02:21 +0100

    form facture, en train de remplir les données d'une facture

---
+++ LFB_gestion/Formulaires/Form_Facture.Designer.cs
@@ -0,0 +1,438 @@
+﻿namespace LFB_gestion.Formulaires
+{
+    partial class Form_Facture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.factureLabel = new System.Windows.Forms.Label();
+            this.numLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CampingName = new System.Windows.Forms.Label();
+            this.adresseLabel = new System.Windows.Forms.Label();
+            this.villeLabel = new System.Windows.Forms.Label();
+            this.telLabel = new System.Windows.Forms.Label();
+            this.mailLabel = new System.Windows.Forms.Label();
+            this.siretLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clientNomLabel = new System.Windows.Forms.Label();
+            this.clientMailLabel = new System.Windows.Forms.Label();
+            this.clientTelLabel = new System.Windows.Forms.Label();
+            this.clientVilleLabel = new System.Windows.Forms.Label();
+            this.clientAdresseLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTextBox = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emplacementTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.régléTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tvaLabel = new System.Windows.Forms.Label();
+            this.ttcLabel = new System.Windows.Forms.Label();
+            this.acompteCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // factureLabel
+            // 
+            this.factureLabel.AutoSize = true;
+            this.factureLabel.Location = new System.Drawing.Point(399, 13);
+            this.factureLabel.Name = "factureLabel";
+            this.factureLabel.Size = new System.Drawing.Size(43, 13);
+            this.factureLabel.TabIndex = 0;
+            this.factureLabel.Text = "Facture";
+            // 
+            // numLabel
+            // 
+            this.numLabel.AutoSize = true;
+            this.numLabel.Location = new System.Drawing.Point(624, 39);
+            this.numLabel.Name = "numLabel";
+            this.numLabel.Size = new System.Drawing.Size(19, 13);
+            this.numLabel.TabIndex = 1;
+            this.numLabel.Text = "N°";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(613, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date";
+            // 
+            // CampingName
+            // 
+            this.CampingName.AutoSize = true;
+            this.CampingName.Location = new System.Drawing.Point(13, 55);
+            this.CampingName.Name = "CampingName";
+            this.CampingName.Size = new System.Drawing.Size(81, 13);
+            this.CampingName.TabIndex = 3;
+            this.CampingName.Text = "Les flots Blancs";
+            // 
+            // adresseLabel
+            // 
+            this.adresseLabel.AutoSize = true;
+            this.adresseLabel.Location = new System.Drawing.Point(13, 68);
+            this.adresseLabel.Name = "adresseLabel";
+            this.adresseLabel.Size = new System.Drawing.Size(45, 13);
+            this.adresseLabel.TabIndex = 4;
+            this.adresseLabel.Text = "Adresse";
+            // 
+            // villeLabel
+            // 
+            this.villeLabel.AutoSize = true;
+            this.villeLabel.Location = new System.Drawing.Point(13, 81);
+            this.villeLabel.Name = "villeLabel";
+            this.villeLabel.Size = new System.Drawing.Size(95, 13);
+            this.villeLabel.TabIndex = 5;
+            this.villeLabel.Text = "Code Postal + Ville";
+            // 
+            // telLabel
+            // 
+            this.telLabel.AutoSize = true;
+            this.telLabel.Location = new System.Drawing.Point(13, 94);
+            this.telLabel.Name = "telLabel";
+            this.telLabel.Size = new System.Drawing.Size(58, 13);
+            this.telLabel.TabIndex = 6;
+            this.telLabel.Text = "Téléphone";
+            // 
+            // mailLabel
+            // 
+            this.mailLabel.AutoSize = true;
+            this.mailLabel.Location = new System.Drawing.Point(13, 107);
+            this.mailLabel.Name = "mailLabel";
+            this.mailLabel.Size = new System.Drawing.Size(32, 13);
+            this.mailLabel.TabIndex = 7;
+            this.mailLabel.Text = "Email";
+            // 
+            // siretLabel
+            // 
+            this.siretLabel.AutoSize = true;
+            this.siretLabel.Location = new System.Drawing.Point(13, 120);
+            this.siretLabel.Name = "siretLabel";
+            this.siretLabel.Size = new System.Drawing.Size(37, 13);
+            this.siretLabel.TabIndex = 8;
+            this.siretLabel.Text = "Siret : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "au";
+            // 
+            // clientNomLabel
+            // 
+            this.clientNomLabel.AutoSize = true;
+            this.clientNomLabel.Location = new System.Drawing.Point(604, 103);
+            this.clientNomLabel.Name = "clientNomLabel";
+            this.clientNomLabel.Size = new System.Drawing.Size(39, 13);
+            this.clientNomLabel.TabIndex = 11;
+            this.clientNomLabel.Text = "Client :";
+            // 
+            // clientMailLabel
+            // 
+            this.clientMailLabel.AutoSize = true;
+            this.clientMailLabel.Location = new System.Drawing.Point(611, 205);
+            this.clientMailLabel.Name = "clientMailLabel";
+            this.clientMailLabel.Size = new System.Drawing.Size(32, 13);
+            this.clientMailLabel.TabIndex = 15;
+            this.clientMailLabel.Text = "Email";
+            // 
+            // clientTelLabel
+            // 
+            this.clientTelLabel.AutoSize = true;
+            this.clientTelLabel.Location = new System.Drawing.Point(585, 179);
+            this.clientTelLabel.Name = "clientTelLabel";
+            this.clientTelLabel.Size = new System.Drawing.Size(58, 13);
+            this.clientTelLabel.TabIndex = 14;
+            this.clientTelLabel.Text = "Téléphone";
+            // 
+            // clientVilleLabel
+            // 
+            this.clientVilleLabel.AutoSize = true;
+            this.clientVilleLabel.Location = new System.Drawing.Point(548, 156);
+            this.clientVilleLabel.Name = "clientVilleLabel";
+            this.clientVilleLabel.Size = new System.Drawing.Size(95, 13);
+            this.clientVilleLabel.TabIndex = 13;
+            this.clientVilleLabel.Text = "Code Postal + Ville";
+            // 
+            // clientAdresseLabel
+            // 
+            this.clientAdresseLabel.AutoSize = true;
+            this.clientAdresseLabel.Location = new System.Drawing.Point(598, 128);
+            this.clientAdresseLabel.Name = "clientAdresseLabel";
+            this.clientAdresseLabel.Size = new System.Drawing.Size(45, 13);
+            this.clientAdresseLabel.TabIndex = 12;
+            this.clientAdresseLabel.Text = "Adresse";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Location du";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(649, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // dateTextBox
+            // 
+            this.dateTextBox.Location = new System.Drawing.Point(649, 61);
+            this.dateTextBox.Name = "dateTextBox";
+            this.dateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.dateTextBox.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(649, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(649, 100);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(649, 176);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 22;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(649, 151);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(649, 202);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Emplacement";
+            // 
+            // emplacementTextBox
+            // 
+            this.emplacementTextBox.Location = new System.Drawing.Point(90, 176);
+            this.emplacementTextBox.Name = "emplacementTextBox";
+            this.emplacementTextBox.Size = new System.Drawing.Size(38, 20);
+            this.emplacementTextBox.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Total ht";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(141, 267);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(92, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Tva";
+            // 
+            // régléTextBox
+            // 
+            this.régléTextBox.Location = new System.Drawing.Point(141, 356);
+            this.régléTextBox.Name = "régléTextBox";
+            this.régléTextBox.Size = new System.Drawing.Size(100, 20);
+            this.régléTextBox.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(92, 359);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Réglé";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(92, 322);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 33;
+            this.label8.Text = "Total ttc";
+            // 
+            // tvaLabel
+            // 
+            this.tvaLabel.AutoSize = true;
+            this.tvaLabel.Location = new System.Drawing.Point(141, 294);
+            this.tvaLabel.Name = "tvaLabel";
+            this.tvaLabel.Size = new System.Drawing.Size(13, 13);
+            this.tvaLabel.TabIndex = 36;
+            this.tvaLabel.Text = "0";
+            // 
+            // ttcLabel
+            // 
+            this.ttcLabel.AutoSize = true;
+            this.ttcLabel.Location = new System.Drawing.Point(141, 322);
+            this.ttcLabel.Name = "ttcLabel";
+            this.ttcLabel.Size = new System.Drawing.Size(48, 13);
+            this.ttcLabel.TabIndex = 37;
+            this.ttcLabel.Text = "tvaLabel";
+            // 
+            // acompteCheckBox
+            // 
+            this.acompteCheckBox.AutoSize = true;
+            this.acompteCheckBox.Location = new System.Drawing.Point(13, 13);
+            this.acompteCheckBox.Name = "acompteCheckBox";
+            this.acompteCheckBox.Size = new System.Drawing.Size(68, 17);
+            this.acompteCheckBox.TabIndex = 38;
+            this.acompteCheckBox.Text = "Acompte";
+            this.acompteCheckBox.UseVisualStyleBackColor = true;
+            this.acompteCheckBox.CheckedChanged += new System.EventHandler(this.acompteCheckBox_CheckedChanged);
+            // 
+            // Form_Facture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 450);
+            this.Controls.Add(this.acompteCheckBox);
+            this.Controls.Add(this.ttcLabel);
+            this.Controls.Add(this.tvaLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.régléTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.emplacementTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dateTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clientMailLabel);
+            this.Controls.Add(this.clientTelLabel);
+            this.Controls.Add(this.clientVilleLabel);
+            this.Controls.Add(this.clientAdresseLabel);
+            this.Controls.Add(this.clientNomLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.siretLabel);
+            this.Controls.Add(this.mailLabel);
+            this.Controls.Add(this.telLabel);
+            this.Controls.Add(this.villeLabel);
+            this.Controls.Add(this.adresseLabel);
+            this.Controls.Add(this.CampingName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numLabel);
+            this.Controls.Add(this.factureLabel);
+            this.Name = "Form_Facture";
+            this.Text = "Form_Facture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label factureLabel;
+        private System.Windows.Forms.Label numLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label CampingName;
+        private System.Windows.Forms.Label adresseLabel;
+        private System.Windows.Forms.Label villeLabel;
+        private System.Windows.Forms.Label telLabel;
+        private System.Windows.Forms.Label mailLabel;
+        private System.Windows.Forms.Label siretLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label clientNomLabel;
+        private System.Windows.Forms.Label clientMailLabel;
+        private System.Windows.Forms.Label clientTelLabel;
+        private System.Windows.Forms.Label clientVilleLabel;
+        private System.Windows.Forms.Label clientAdresseLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox dateTextBox;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox emplacementTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox régléTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label tvaLabel;
+        private System.Windows.Forms.Label ttcLabel;
+        private System.Windows.Forms.CheckBox acompteCheckBox;
+    }
+}
---
+++ LFB_gestion/Formulaires/Form_Facture.cs
@@ -0,0 +1,54 @@
+﻿using LFB_gestion.Entités;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LFB_gestion.Formulaires
+{
+    public partial class Form_Facture : Form
+    {
+        private SqlConnection connexion = Outils.Connexion();
+
+        public Form_Facture(Entite_Reservation réservation)
+        {
+            InitializeComponent();
+            remplirDonnées();
+        }
+
+        private void remplirDonnées()
+        {
+            SqlCommand cmd = new SqlCommand("select count(*) from facture", connexion);
+            try
+            {
+                connexion.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                int numFacture = reader.GetInt32(0) + 1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void acompteCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (acompteCheckBox.Checked)
+            {
+                acompteCheckBox.Text = "Facture d'acompte";
+            }
+            else
+            {
+                acompteCheckBox.Text = "Facture";
+            }
+        }
+    }
+}
