a77a054722bb332e268c472e9058ec18bbaf5de6
bdimchev <bdimchev@INFO-207-12>
2022-03-22 17:12:37 +0100

    tout le back du form facture est ok :)), reste à ajouter la somme au pdf

--- LFB_gestion/Formulaires/Form_Facture.cs
+++ LFB_gestion/Formulaires/Form_Facture.cs
@@ -25,6 +25,9 @@ namespace LFB_gestion.Formulaires
         {
             this.réservation = réservation;
             InitializeComponent();
+            string[] tab = new string[] { "5.5", "10", "20" };
+            tvaComboBox.Items.AddRange(tab);
+            tvaComboBox.SelectedIndex = 1;
             remplirDonnées();
         }
 
@@ -50,6 +53,55 @@ namespace LFB_gestion.Formulaires
             finTextBox.Text = réservation.fin.ToString("dd/MM/yyyy");
 
             emplacementTextBox.Text = réservation.emplacement.ToString();
+
+            //Remplir la première ligne du dataGrid avec la location comme désignaiton
+            DataGridViewRow row = (DataGridViewRow)dataGridView.Rows[0].Clone();
+            row.Cells[0].Value = "Location de l'emplacement" + emplacementTextBox.Text + " du " + débutTextBox.Text + " au " + finTextBox.Text;
+            TimeSpan temps = réservation.fin - réservation.début;
+            double jours = temps.TotalDays;
+            double somme = 50 * jours; //On dit qu'un jour coûte 50€
+            row.Cells[1].Value = somme + " €";
+            dataGridView.Rows.Add(row);
+
+            calculer();
+        }
+
+        /// <summary>
+        /// Calcule et met à jour les valeurs des montants ht tva ttc
+        /// </summary>
+        private void calculer()
+        {
+            double ht = 0;
+            double tva = 0;
+            //somme ht de toutes les désignations
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                if (!isLastRow(row))
+                {
+                    string[] tab = row.Cells[1].Value.ToString().Split(' ');
+                    try
+                    {
+                        ht += double.Parse(tab[0]);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Conversion impossible du montant : " + row.Cells[1].Value.ToString());
+                    }
+                }
+            }
+            totalHtTextBox.Text = ht.ToString();
+            //Établir le montant tva
+            try
+            {
+                tva = ht + (ht * double.Parse(tvaComboBox.SelectedItem.ToString()) / 100);
+                tvaTextBox.Text = tva.ToString();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Impossible de convertir la valeur de tva en un numéro");
+            }
+            ttcLabel.Text = (ht + tva).ToString();
+            régléTextBox.Text = ttcLabel.Text;
         }
 
         /// <summary>
@@ -109,6 +161,11 @@ namespace LFB_gestion.Formulaires
             }
         }
 
+        /// <summary>
+        /// Génération du pdf
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void validerButton_Click(object sender, EventArgs e)
         {
             //Création du document
@@ -127,14 +184,21 @@ namespace LFB_gestion.Formulaires
             Font titre = new Font(iTextSharp.text.Font.FontFamily.HELVETICA, 20f, iTextSharp.text.Font.BOLD, blue);
             Font titreRed = new Font(iTextSharp.text.Font.FontFamily.HELVETICA, 20f, iTextSharp.text.Font.BOLD, red);
             Font policeTh = new Font(iTextSharp.text.Font.FontFamily.HELVETICA, 16f, 1, blanc);
-            Font normal = new Font(iTextSharp.text.Font.FontFamily.HELVETICA, 14f, 1, noir);
+            Font normal = new Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12f, 1, noir);
 
 
             //Page
             //Titre
-            Paragraph facture = new Paragraph("Facture\n\n", titre);
+            Paragraph facture = new Paragraph(factureLabel.Text + "\n\n", titre);
             facture.Alignment = Element.ALIGN_CENTER;
+
+            Paragraph num = addParagraph("N°" + numFactureTextBox.Text, normal);
+            Paragraph date = addParagraph("Date " + dateTextBox.Text, normal);
+            num.Alignment = Element.ALIGN_RIGHT;
+            date.Alignment = Element.ALIGN_RIGHT;
             doc.Add(facture);
+            doc.Add(num);
+            doc.Add(date);
 
             #region Données entreprise
             List<Paragraph> tabE = new List<Paragraph>();
@@ -160,6 +224,7 @@ namespace LFB_gestion.Formulaires
             tabCl.Add(addParagraph(villeTextBox.Text, normal));
             tabCl.Add(addParagraph(telTextBox.Text, normal));
             tabCl.Add(addParagraph(mailTextBox.Text, normal));
+            tabCl.Add(new Paragraph("\n"));
 
             foreach (Paragraph p in tabCl)
             {
@@ -169,15 +234,96 @@ namespace LFB_gestion.Formulaires
             #endregion
 
             #region Désignations
-            Paragraph loc = new Paragraph();
+            //Construction du tableau
+            PdfPTable table = new PdfPTable(2);
+            table.SetWidthPercentage(new float[] { 75f, 25f }, doc.PageSize); //Pourcentage de largeur pourles colonnes
+            table.WidthPercentage = 100; //Occupation du tableau dans le pdf
+            //La colonne désignations
+            PdfPCell desCell = new PdfPCell(new Phrase("Désignation", policeTh));
+            desCell.BackgroundColor = blue;
+            desCell.Padding = 7;
+            desCell.BorderColor = blue;
+            table.AddCell(desCell);
+            //La colonne montant ht
+            PdfPCell montantCell = new PdfPCell(new Phrase("Montant HT", policeTh));
+            montantCell.BackgroundColor = blue;
+            montantCell.Padding = 7;
+            montantCell.BorderColor = blue;
+            table.AddCell(montantCell);
+
+            //Ajouter les lignes du dataGridView dans le pdf
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                if (!isLastRow(row))
+                {
+                    if (row.Cells[0].Value != null || row.Cells[1].Value != null)
+                    {
+                        addCellTab(row.Cells[0].Value.ToString(), normal, gris, table);
+                        addCellTab(row.Cells[1].Value.ToString(), normal, gris, table);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Toutes les colonnes doivent être remplies");
+                    }
+                }
+            }
+
+            //Afficher le total
+
+            doc.Add(table);
             #endregion
 
             doc.Close();
         }
 
+        /// <summary>
+        /// Ajoute une cellule dans un tableau
+        /// </summary>
+        /// <param name="s">le texte à mettre deans la cellule</param>
+        /// <param name="f">la fonte pour le texte</param>
+        /// <param name="c">une couleur</param>
+        /// <param name="t">le tableau</param>
+        private void addCellTab(string s, Font f, BaseColor c, PdfPTable t)
+        {
+            PdfPCell cell = new PdfPCell(new Phrase(s));
+            cell.BackgroundColor = c;
+            cell.Padding = 7;
+            cell.BorderColor = new BaseColor(240, 240, 240);
+            t.AddCell(cell);
+        }
+
+        /// <summary>
+        /// Créer un nouveau paragraphe pour le pdf
+        /// </summary>
+        /// <param name="s">le texte à mettre dans le paragraphe</param>
+        /// <param name="font">la font pour le paragraphe</param>
+        /// <returns>renvoie un paragraphe</returns>
         private Paragraph addParagraph(string s, Font font)
         {
             return new Paragraph(s + "\n", font);
+        }
+
+        /// <summary>
+        /// Vérifie si c'est la dernière ligne car la dernière ligne d'un dataGridView est toujours vide
+        /// </summary>
+        /// <param name="row">la ligne courante</param>
+        /// <returns></returns>
+        private bool isLastRow(DataGridViewRow row)
+        {
+            return (row.Index == dataGridView.Rows.Count - 1);
+        }
+
+        /// <summary>
+        /// Actualise les calucls des montants ht tva ttc
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView_CellValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1)
+            {
+                calculer();
+            }
         }
     }
 }
