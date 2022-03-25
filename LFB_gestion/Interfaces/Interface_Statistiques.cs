951cba8b085221fe1e87484cbc43374715152da3
sebcandau <candau.sebastien@laposte.net>
2022-03-25 11:22:16 +0100

    Affichage des stats principales fonctionnel :white_check_mark:

--- LFB_gestion/Interfaces/Interface_Statistiques.cs
+++ LFB_gestion/Interfaces/Interface_Statistiques.cs
@@ -17,26 +17,67 @@ namespace LFB_gestion.Interfaces
         public Interface_Statistiques()
         {
             nomModuleLabel.Text = "Module statistiques";
+            InitializeComponent();
             afficherProduitLePlusVendu();
             afficherPlusGrosConsommateur();
-            InitializeComponent();
+            afficherEmplacementAvecLePlusIncidents();
+            afficheremplacementPrefere();
+        }
+
+
+
+        private void afficheremplacementPrefere()
+        {
+            connexion.Open();
+            SqlCommand idQuery = new SqlCommand("select nom,COUNT(id_emplacement) as nb_reserves from emplacement inner join reservation on emplacement.id = reservation.id_emplacement GROUP BY nom  ORDER BY nb_reserves DESC OFFSET 0 ROWS FETCH FIRST 1 ROWS ONLY", connexion);
+            SqlDataReader rd;
+            Label label = new Label();
+            label.Size = new Size(200, 50);
+            label.Location = new Point(500, 50);
+            rd = idQuery.ExecuteReader();
+
+            while (rd.Read())
+            {
+                label.Text = "L'emplacement préféré des clients est\n l'" + rd["nom"].ToString() + " avec " + rd["nb_reserves"].ToString() + " réservation(s)";
+            }
+            connexion.Close();
+            this.clientsPanel.Controls.Add(label);
+        }
+
+        private void afficherEmplacementAvecLePlusIncidents()
+        {
+            connexion.Open();
+            SqlCommand idQuery = new SqlCommand("select emplacement.nom,COUNT(incident.id) as nb_incidents from emplacement inner join incident on emplacement.id = incident.id_emplacement group by emplacement.nom ORDER BY nb_incidents DESC OFFSET 0 ROWS FETCH FIRST 1 ROWS ONLY", connexion);
+            SqlDataReader rd;
+            Label label = new Label();
+            label.Size = new Size(200, 50);
+            label.Location = new Point(900, 50);
+            rd = idQuery.ExecuteReader();
+
+            while (rd.Read())
+            {
+                label.Text = "L'emplacement avec le plus d'incidents est \n l'" + rd["nom"].ToString() + " avec " + rd["nb_incidents"].ToString() + " incidents";
+            }
+            connexion.Close();
+            this.clientsPanel.Controls.Add(label);
         }
 
         private void afficherPlusGrosConsommateur()
         {
             connexion.Open();
-            SqlCommand idQuery = new SqlCommand("select produit.nom,COUNT(vente.id) as nb_vente from produit inner join vente on produit.id = vente.id_produit group by produit.nom ORDER BY nb_vente DESC OFFSET 0 ROWS FETCH FIRST 1 ROWS ONLY", connexion);
+            SqlCommand idQuery = new SqlCommand("select client.nom,client.prenom,COUNT(vente.id) as nb_vente from client inner join vente on client.id = vente.id_client group by client.nom, client.prenom ORDER BY nb_vente DESC OFFSET 0 ROWS FETCH FIRST 1 ROWS ONLY", connexion);
             SqlDataReader rd;
+            Label label = new Label();
+            label.Size = new Size(200, 50);
+            label.Location = new Point(1200, 50);
             rd = idQuery.ExecuteReader();
-            List<Control> elements = new List<Control>();
 
             while (rd.Read())
             {
-                Entités.Entite_Produit produit = new Entités.Entite_Produit(rd["nom"].ToString(), rd["nb_vente"].ToString());
-                elements.Add(produit);
+                label.Text = "Le plus gros consommateur est " + rd["nom"] + " avec \n" + rd["nb_vente"].ToString() + " ventes";
             }
-            affichageStats(elements);
             connexion.Close();
+            this.clientsPanel.Controls.Add(label);
         }
 
         private void afficherProduitLePlusVendu()
@@ -67,13 +108,13 @@ namespace LFB_gestion.Interfaces
                 foreach (Control element in elements)
                 {
                     if (element == elements[0])
-                    { 
+                    {
                         Label meilleureVenteLabel = new Label();
                         meilleureVenteLabel.Text = "Produit le plus vendu : ";
                         meilleureVenteLabel.Size = new Size(400, 40);
                         meilleureVenteLabel.Location = new Point(20, 20);
                         this.clientsPanel.Controls.Add(meilleureVenteLabel);
-                        element.Location = new Point(20,30);
+                        element.Location = new Point(20, 30);
                     }
                     else
                     {
@@ -89,6 +130,11 @@ namespace LFB_gestion.Interfaces
                 MessageBox.Show("Pas de client dans la base");
             }
             connexion.Close();
+        }
+
+        private void rechercheBouton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
