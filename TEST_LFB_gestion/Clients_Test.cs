ca4fc5ec7d73854da1617b1afeec9507edde6c6f
bdimchev <bdimchev@INFO-207-12>
2022-03-29 15:54:34 +0200

    maj test ajoutClient

--- TEST_LFB_gestion/Clients_Test.cs
+++ TEST_LFB_gestion/Clients_Test.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data.Common;
 using System.Data.SqlClient;
-using static LFB_gestion.Formulaires.Form_Client;
 
 namespace TEST_LFB_gestion
 {
@@ -10,78 +9,21 @@ namespace TEST_LFB_gestion
     {
         SqlConnection connexion = LFB_gestion.Outils.Connexion();
         [TestMethod]
-        public void testsChiffrage()
+        public void testAjoutClient()
         {
-            // On créé une chaine de caractères
-            string s = "motdepasse";
-            // On crypte la chaine de caractères
-           string sCrypte = LFB_gestion.Outils.chiffrer(s);
-            // On vérifie que la chaine de départ (s) est différente de sa version chiffré
-            Assert.AreNotEqual(s, sCrypte);
-
-            // On vérifie si le chiffrage fonctionne sur une chaine complexe
-            string sComplexe = "aMoR78_@&é'==SSi, P";
-            sCrypte = LFB_gestion.Outils.chiffrer(sComplexe);
-            Assert.AreNotEqual(sComplexe, sCrypte);
-
-
-        }
-
-        [TestMethod]
-        public void testsMailEstValide()
-        {
-            // Sur adresse mail valide
-            // On créer plusieurs adresses mails valide
-            string valide = "tonyStark@gmail.com";
-            string valideAvecTiret = "tony-stark@gmail.com";
-            string valideAvecPoint = "tony-stark@gmail.com";
-            string valideAvecChiffre = "tony2-stark@gmail.com";
-            Assert.IsTrue(LFB_gestion.Outils.mailEstValide(valide));
-            Assert.IsTrue(LFB_gestion.Outils.mailEstValide(valideAvecTiret));
-            Assert.IsTrue(LFB_gestion.Outils.mailEstValide(valideAvecChiffre));
-            Assert.IsTrue(LFB_gestion.Outils.mailEstValide(valideAvecPoint));
-
-            // sur adresse mail invalide
-            string nonValideAvecEspace = "tony stark@gmail.com";
-            string nonValideSansArobase = "tonystarkgmail.com";
-            string nonValideSansDomaine = "tonystark@gmail";
-            Assert.IsFalse(LFB_gestion.Outils.mailEstValide(nonValideAvecEspace));
-           // Assert.IsFalse(LFB_gestion.Outils.mailEstValide(nonValideAvecCaractereSpecial));
-            Assert.IsFalse(LFB_gestion.Outils.mailEstValide(nonValideSansArobase));
-           // Assert.IsFalse(LFB_gestion.Outils.mailEstValide(nonValideAvecTiretApresArobase));
-            Assert.IsFalse(LFB_gestion.Outils.mailEstValide(nonValideSansDomaine));
-
-
-        }
-
-        [TestMethod]
-        public void trouverClient()
-        {
-            // On créé un client dans la base
-            string nom = "bob";
-            string prenom = "dupont";
-            string mail = "bob.dupont@laposte.net";
-            int id = 0;
-            SqlConnection conn = LFB_gestion.Outils.Connexion();
-            string query = "select max (id) from client";
-            SqlCommand command = new SqlCommand(query, conn);
-            DbDataReader reader = command.ExecuteReader();
+            LFB_gestion.Classes.ClientMéthodes.creationDuClient("nom", "prénom", "mail", "adresse", "code postal", "ville", "telephone");
+            string query = "select * from client " +
+                "where nom='nom' and prenom='prenom' and mail='mail' and adresse='adresse' and codePostal='code postal' and ville='ville' and telephone='telephone'";
+            SqlCommand cmd = new SqlCommand(query, connexion);
+            DbDataReader reader = cmd.ExecuteReader();
             if (reader.Read())
             {
-                id = reader.GetInt32(0);
-                id++;
-                reader.Close();
+                Assert.IsTrue(true);
             }
-                query = "insert into client values (@id, @nom, @prenom, @mail)";
-                command = new SqlCommand(query, conn);
-                command.Parameters.AddWithValue("@id", id);
-                command.Parameters.AddWithValue("@nom", nom);
-                command.Parameters.AddWithValue("@prenom", prenom);
-                command.Parameters.AddWithValue("@mail", mail);
-                command.ExecuteNonQuery();
-
-            // Une fois le client créé on teste la méthode trouverClient
-            Assert.AreEqual(LFB_gestion.Outils.trouverClient(id).nom, "nom");
+            else
+            {
+                Assert.IsTrue(false);
+            }
         }
     }
 }
