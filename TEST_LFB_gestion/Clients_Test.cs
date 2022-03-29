493f5fdb152c6a345339e98c46899997ddf9802f
bdimchev <bdimchev@INFO-207-12>
2022-03-29 15:43:25 +0200

    Début de tests sur client, problème avec les using SQL

--- TEST_LFB_gestion/Clients_Test.cs
+++ TEST_LFB_gestion/Clients_Test.cs
@@ -6,10 +6,14 @@ namespace TEST_LFB_gestion
     [TestClass]
     public class Clients_Test
     {
+        SqlConnection connexion = LFB_gestion.Outils.Connexion();
         [TestMethod]
         public void TestCreationClients()
         {
-            LFB_gestion.Outils.crypter("hey");
+            LFB_gestion.Classes.ClientMéthodes.creationDuClient("nom", "prénom", "mail", "adresse", "code postal", "ville", "telephone");
+            string query = "select * from client " +
+                "where nom='nom' and prenom='prenom' and mail='mail' and adresse='adresse' and codePostal='code postal' and ville='ville' and telephone='telephone'";
+            SqlCommand cmd = new SqlCommand();
         }
     }
 }
