ae0795c006e4cb9a898606444593421bdc9d517f
bdimchev <bdimchev@INFO-207-12>
2022-03-29 14:14:44 +0200

    maj pour mettre dans le main

--- TEST_LFB_gestion/Clients_Test.cs
+++ TEST_LFB_gestion/Clients_Test.cs
@@ -9,30 +9,7 @@ namespace TEST_LFB_gestion
         [TestMethod]
         public void TestCreationClients()
         {
-            Assert.IsTrue(true);
-            LFB_gestion.Formulaires.Form_Client.TestCreationClient("","","");
-            
-        }
-
-        [TestMethod]
-        public void connexionAffichageClients()
-        {
-            // On créé une interface client
-            LFB_gestion.Interfaces.Interface_Clients interfaceClients = new LFB_gestion.Interfaces.Interface_Clients();
-            // On créé une liste de clients
-            List<LFB_gestion.Entités.Entite_Client> clients = new List<LFB_gestion.Entités.Entite_Client>();
-
-            // On la remplie avec des clients provisoires (non inscrits dans la base)
-
-            //client 1
-            LFB_gestion.Entités.Entite_Client client1 = new LFB_gestion.Entités.Entite_Client("Michel", "Dumas", "chefMichel@gmail.com");
-            //client 2
-            LFB_gestion.Entités.Entite_Client client2 = new LFB_gestion.Entités.Entite_Client("Eric", "Cartman", "eric@gmail.com");
-            //ajout des clients dans la liste
-            clients.Add(client1);
-            clients.Add(client2);
-
-
+            LFB_gestion.Outils.crypter("hey");
         }
     }
 }
